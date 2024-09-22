6121570237ad86fc7c9f2b407d4fd9cc8292937a
Ahmed <noxioustab@gmail.com>
2024-09-22 14:00:49 +0530

    Analyze these changes, understand the django structure

--- security_enhancer/urls.py
+++ security_enhancer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.mainpage, name='mainpage'),
     path('upload/', views.upload_apk, name='upload_apk'),
     path('download/', views.download_page, name='download_page'),
 ]
